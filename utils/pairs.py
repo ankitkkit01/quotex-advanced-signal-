2b3c2fe41967af9bf26726bc3061217e84f3df21
ankitkkit01 <akhimanshya@gmail.com>
2025-06-22 22:18:27 +0530

    Create pairs.py

---
+++ utils/pairs.py
@@ -0,0 +1,18 @@
+all_pairs = [
+    # Forex Major & Minor (OTC)
+    "EUR/USD OTC", "GBP/USD OTC", "USD/JPY OTC", "AUD/USD OTC", "NZD/USD OTC", "USD/CHF OTC", "EUR/GBP OTC", "USD/CAD OTC",
+    "EUR/JPY OTC", "GBP/JPY OTC", "AUD/JPY OTC", "EUR/CAD OTC", "EUR/CHF OTC", "AUD/CHF OTC", "GBP/AUD OTC", "NZD/CHF OTC",
+    "NZD/CAD OTC", "AUD/NZD OTC",
+
+    # Exotic Forex (OTC)
+    "USD/PKR OTC", "USD/IDR OTC", "USD/INR OTC", "USD/MXN OTC", "USD/ZAR OTC", "USD/BRL OTC", "USD/TRY OTC", "USD/COP OTC",
+
+    # Crypto (OTC)
+    "BTC/USD OTC", "ETH/USD OTC", "LTC/USD OTC", "SOL/USD OTC", "XRP/USD OTC", "DOGE/USD OTC", "SHIBA/USD OTC", "TON/USD OTC", "TRUMP/USD OTC",
+
+    # Commodities (OTC)
+    "GOLD/USD OTC", "SILVER/USD OTC",
+
+    # Indices (OTC)
+    "UK100/USD OTC"
+]
