366fe86b0d91c543c05bef7439cd03121c71ff03
ankitkkit01 <akhimanshya@gmail.com>
2025-06-23 00:25:52 +0530

    Update time_utils.py

--- utils/time_utils.py
+++ utils/time_utils.py
@@ -1,17 +1,13 @@
-# utils/time_utils.py
-
 from datetime import datetime, timedelta
 
 def get_current_ist_time():
-    """
-    Returns current time in IST (UTC+5:30)
-    """
     return datetime.utcnow() + timedelta(hours=5, minutes=30)
 
+def get_next_minute_entry_time():
+    now = get_current_ist_time()
+    next_minute = now + timedelta(minutes=1)
+    return next_minute.strftime("%H:%M")
+
 def is_exact_time():
-    """
-    Returns True if current IST time's seconds == 0
-    Useful for signaling exactly on start of minute
-    """
     now = get_current_ist_time()
     return now.second == 0
