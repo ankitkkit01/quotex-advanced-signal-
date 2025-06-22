a5ceb471b59637d5f1183b0e14554f776dbaa595
ankitkkit01 <akhimanshya@gmail.com>
2025-06-22 23:01:08 +0530

    Update time_utils.py

--- utils/time_utils.py
+++ utils/time_utils.py
@@ -1,7 +1,18 @@
-# utils/time_utils.py
+from datetime import datetime, timedelta
 
-import datetime
+def get_entry_time(offset_minutes=1):
+    """
+    Returns the entry time in IST (UTC+5:30) in HH:MM format.
+    
+    :param offset_minutes: How many minutes ahead from the current time you want
+    """
+    ist_time = datetime.utcnow() + timedelta(hours=5, minutes=30)
+    entry_time = ist_time + timedelta(minutes=offset_minutes)
+    return entry_time.strftime('%H:%M')
 
-def is_exact_time(interval_sec=10):
-    now = datetime.datetime.utcnow()
-    return (now.second % interval_sec) == 0
+def get_current_ist_time():
+    """
+    Returns the current IST time in HH:MM format.
+    """
+    ist_time = datetime.utcnow() + timedelta(hours=5, minutes=30)
+    return ist_time.strftime('%H:%M')
