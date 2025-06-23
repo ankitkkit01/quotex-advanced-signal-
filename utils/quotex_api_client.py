790aa2bb619b6cdc39b6a62d08125b733324c340
ankitkkit01 <akhimanshya@gmail.com>
2025-06-24 01:47:55 +0530

    Create quotex_api_client.py

---
+++ utils/quotex_api_client.py
@@ -0,0 +1,19 @@
+from quotexapi.client import Quotex
+
+EMAIL = "akhimanshya@gmail.com"       # Quotex login email
+PASSWORD = "ankit7492" # Quotex login password
+
+def get_client():
+    client = Quotex(EMAIL, PASSWORD)
+    client.connect()
+    if client.check_connect():
+        print("✅ Connected to Quotex")
+        return client
+    else:
+        raise Exception("❌ Failed to connect")
+
+def get_balance(client):
+    return client.get_balance()
+
+def get_payout(client, asset):
+    return client.get_all_profit().get(asset, 0)
