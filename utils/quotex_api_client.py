913219f6c0d86f06b8254ae47d4b2c810e3fac10
ankitkkit01 <akhimanshya@gmail.com>
2025-06-24 03:48:03 +0530

    Update quotex_api_client.py

--- utils/quotex_api_client.py
+++ utils/quotex_api_client.py
@@ -1,19 +1,9 @@
 from quotexapi.client import Quotex
 
-EMAIL = "arhimanshya@gmail.com"       # Quotex login email
-PASSWORD = "12345678an" # Quotex login password
-
-def get_client():
-    client = Quotex(EMAIL, PASSWORD)
+def get_client(arhimanshya@gmail.com):
+    client = Quotex(12345678an)
     client.connect()
-    if client.check_connect():
-        print("✅ Connected to Quotex")
-        return client
-    else:
-        raise Exception("❌ Failed to connect")
-
-def get_balance(client):
-    return client.get_balance()
+    return client
 
 def get_payout(client, asset):
-    return client.get_all_profit().get(asset, 0)
+    return client.get_all_profit()[asset]["turbo"]
