9d12d5a84cfafe9eb357394dd990326d098273b4
ankitkkit01 <akhimanshya@gmail.com>
2025-06-23 19:53:40 +0530

    Update time_utils.py

--- utils/time_utils.py
+++ utils/time_utils.py
@@ -1,13 +1,8 @@
+# utils/time_utils.py
+
 from datetime import datetime, timedelta
 
-def get_current_ist_time():
-    return datetime.utcnow() + timedelta(hours=5, minutes=30)
-
-def get_next_minute_entry_time():
-    now = get_current_ist_time()
-    next_minute = now + timedelta(minutes=1)
-    return next_minute.strftime("%H:%M")
-
-def is_exact_time():
-    now = get_current_ist_time()
-    return now.second == 0
+def get_adjusted_entry_time():
+    now = datetime.utcnow() + timedelta(minutes=1)
+    adjusted_time = now.replace(second=30, microsecond=0)
+    return adjusted_time.strftime("%H:%M:%S")
