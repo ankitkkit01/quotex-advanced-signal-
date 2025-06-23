a8798d8ba2102a124f50d8d034e6ba7606938dc3
ankitkkit01 <akhimanshya@gmail.com>
2025-06-23 20:22:15 +0530

    Update time_utils.py

--- utils/time_utils.py
+++ utils/time_utils.py
@@ -1,7 +1,8 @@
-from datetime import datetime, timedelta
+import datetime
+import pytz
 
-def get_adjusted_entry_time():
-    ist_now = datetime.utcnow() + timedelta(hours=5, minutes=30)
-    entry_time = ist_now + timedelta(minutes=1)
-    adjusted_time = entry_time.replace(second=5, microsecond=0)
-    return adjusted_time.strftime("%H:%M:%S")
+def get_adjusted_entry_time(seconds_before=15):
+    now = datetime.datetime.now(pytz.timezone("Asia/Kolkata"))
+    next_minute = now + datetime.timedelta(minutes=1)
+    entry_time = next_minute.replace(second=0, microsecond=0) - datetime.timedelta(seconds=seconds_before)
+    return entry_time.strftime("%H:%M:%S")
