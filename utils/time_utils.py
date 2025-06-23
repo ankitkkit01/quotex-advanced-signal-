c1e1491bc045601d7140c4775bb97a82b02a2e5e
ankitkkit01 <akhimanshya@gmail.com>
2025-06-23 20:51:00 +0530

    Update time_utils.py

--- utils/time_utils.py
+++ utils/time_utils.py
@@ -1,3 +1,8 @@
+# utils/time_utils.py
+
+import datetime
+import pytz
+
 def get_future_entry_time(mins_ahead=1):
     now = datetime.datetime.now(pytz.timezone("Asia/Kolkata"))
     next_entry = (now + datetime.timedelta(minutes=mins_ahead)).replace(second=0, microsecond=0)
